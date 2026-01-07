3eaa9e41068602460c8096e625133b073fabfc12
Hernando Abella <hernandoabella@gmail.com>
2026-01-07 14:45:27 -0500

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Header from "@/app/components/Header"
 import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -8,9 +7,7 @@ import Gallery from "./components/Gallery";
 import Testimonials from "./components/Testimonials";
 import ServiceArea from "./components/ServiceArea";
 import Faq from "./components/Faq";
-import CTA from "./components/CTA";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 
 export default function Home() {
   return (
